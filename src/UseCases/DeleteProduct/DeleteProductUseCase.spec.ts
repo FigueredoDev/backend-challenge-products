da7c6d666153be544942b8a8d3b66e708cb93bc7
JhonataFigueredo <jhowfigueredo2@gmail.com>
2023-10-07 10:32:06 -0300

    test: Add unit tests for Products/DeleteProductUseCase

---
+++ src/UseCases/DeleteProduct/DeleteProductUseCase.spec.ts
@@ -0,0 +1,58 @@
+import { beforeEach, describe, expect, it, jest } from "bun:test";
+import { ProductRepositoryMockInterface } from "../../mocks/ProductRepositoryMockInterface";
+import { DeleteProductUseCase } from "./DeleteProductUseCase";
+import { IDeleteProduct } from "./IDeleteProduct";
+import { BusinessError } from "../../errors/BusinessError";
+
+describe("DeleteProductUseCase", () => {
+  let productRepositoryMock: ProductRepositoryMockInterface;
+  let deleteProductUseCase: IDeleteProduct;
+
+  beforeEach(() => {
+    productRepositoryMock = {
+      findAll: jest.fn(),
+      findById: jest.fn(),
+      findByName: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    };
+
+    deleteProductUseCase = new DeleteProductUseCase(productRepositoryMock);
+  });
+
+  it("should delete a product when um valid id is provided", async () => {
+    const id = "some-uuid";
+
+    productRepositoryMock.findById.mockResolvedValue({
+      id,
+      name: "Product name",
+      description: "Product description",
+      price: 100,
+      stock: 10,
+    });
+
+    await deleteProductUseCase.execute(id);
+
+    expect(productRepositoryMock.findById).toHaveBeenCalled();
+    expect(productRepositoryMock.delete).toHaveBeenCalled();
+  });
+
+  it("should throw an error if product not found", async () => {
+    const id = "non-existent-id";
+
+    try {
+      await deleteProductUseCase.execute(id);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BusinessError);
+
+      if (error instanceof BusinessError) {
+        expect(error.message).toBe("Product not found");
+      }
+
+      return;
+    }
+
+    throw new Error("Expected an error to be thrown");
+  });
+});
